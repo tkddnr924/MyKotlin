b23b3415837bfb4aa896ca70f0c4f359fe8f9d77
tkddnr924 <tkddnr924@naver.com>
2021-01-26 01:48:29 +0900

    Use When & is

--- src/main/kotlin/Color/Color.kt
+++ src/main/kotlin/Color/Color.kt
@@ -1,5 +1,5 @@
 enum class Color (val r: Int, val g: Int, val b: Int) {
-    RED(255, 0, 0), ORANGE(255, 165, 0);
+    RED(255, 0, 0), ORANGE(255, 165, 0), YELLOW(255, 255, 0);
 
     fun rgb () = (r * 256 + g) * 256 + b
 }
